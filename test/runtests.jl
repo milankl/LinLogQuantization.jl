b943bfee14889b084947343a9e720ec16d5b0e1f
Pablo Valdunciel <pablo.valdunciel@docyet.com>
2024-10-08 08:30:01 +0200

    fix: set both extrema to positive values

--- test/runtests.jl
+++ test/runtests.jl
@@ -48,7 +48,7 @@ end
     end
 
     @testset "Custom extrema" begin 
-        for ext in [(-0.8, 0.6), (-0.6, 0.75), (-0.5, 0.5)]
+        for ext in [(0.1, 0.6), (0.22, 0.75), (0.3, 0.5)]
             for T in [Float64, Float32, Float16]
                 for s in [(100,),
                             (10,20),
@@ -74,6 +74,7 @@ end
 
                         # then test whether back&forth conversion is reversible
                         @test A2 == Array{T}(LinQuantArray{U}(A2; extrema=ext))
+                        @test Base.extrema(A2) == ext
                     end
                 end
             end
@@ -110,31 +111,38 @@ end
 end
 
 @testset "LinQuant along dimension" begin
-    A = rand(Float32,10,20,30,40)
-    Q = LinQuant32Array(A,4)
-    @test A ≈ Array{Float32}(Q)
+    @testset "Default extrema" begin
+        A = rand(Float32,10,20,30,40)
+        Q = LinQuant32Array(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuant24Array(A,4)
-    @test A ≈ Array{Float32}(Q)
+        Q = LinQuant24Array(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuant16Array(A,4)
-    @test A ≈ Array{Float32}(Q)
+        Q = LinQuant16Array(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuant8Array(A,4)
-    @test all(isapprox.(A,Array{Float32}(Q),atol=1e-1))
+        Q = LinQuant8Array(A,4)
+        @test all(isapprox.(A,Array{Float32}(Q),atol=1e-1))
 
-    A = rand(Float32,10,20,30,40)
-    Q = LinQuantArray{Int32}(A,4)
-    @test A ≈ Array{Float32}(Q)
+        A = rand(Float32,10,20,30,40)
+        Q = LinQuantArray{Int32}(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuantArray{Int24}(A,4)
-    @test A ≈ Array{Float32}(Q)
+        Q = LinQuantArray{Int24}(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuantArray{Int16}(A,4)
-    @test A ≈ Array{Float32}(Q)
+        Q = LinQuantArray{Int16}(A,4)
+        @test A ≈ Array{Float32}(Q)
 
-    Q = LinQuantArray{Int8}(A,4)
-    @test all(isapprox.(A,Array{Float32}(Q),atol=1e-1))
+        Q = LinQuantArray{Int8}(A,4)
+        @test all(isapprox.(A,Array{Float32}(Q),atol=1e-1))
+    end 
+
+    @testset "Custom extrema" begin
+
+
+    end
 end
 
 @testset "LogQuant along dimension" begin
